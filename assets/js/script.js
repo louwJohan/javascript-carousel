f3dc31d8e28fe809912da5b354f4cacae4703e36
Louw Johan Coetzee <Louwjohanc@gmail.com>
2021-12-20 21:35:47 +0000

    Add javacode to start carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 let carouselSlides = document.getElementsByClassName("carousel");
-
+let nextButton = document.getElementsByClassName('button_right');
+let prevButton = document.getElementsByClassName('button_left')
 let slideWidth = carouselSlides[0].clientWidth;
 
+for (let i = 0; i < carouselSlides.length; i++){
+    carouselSlides[i].style.left = slideWidth * i +'px';
+}
 
-
+// Button to move carousel right//
+nextButton.addEventListener('click', e => {
+    let currentSlide = document.getElementsByClassName('current_slide');
+    console.log(currentSlide);
+ } 
+)
