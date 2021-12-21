6c8ae44e00f564300515f1dce1ad2e8f59f002d8
Louw Johan Coetzee <Louwjohanc@gmail.com>
2021-12-21 22:21:42 +0000

    Add code to make left and right buttons work and add style to slides

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,29 @@ let carouselSlides = document.getElementsByClassName("carousel");
 let nextButton = document.querySelector('.button_right');
 let prevButton = document.querySelector('.button_left')
 let slideWidth = carouselSlides[0].clientWidth;
-let caroTrack = document.querySelector('.carousel_track');
+let carouselTrack = document.querySelector('.carousel_track');
 
 
 for (let i = 0; i < carouselSlides.length; i++){
    carouselSlides[i].style.left = slideWidth * i +'px';
 }
 
+// Button make carousel move left//
+prevButton.addEventListener('click', e => {
+   let currentSlide = document.querySelector('.current_slide');
+   let nextSlide = currentSlide.previousElementSibling;
+   let moveAmount = nextSlide.style.left;
+   carouselTrack.style.transform = 'translateX(-'+ moveAmount +')';
+   currentSlide.classList.remove('current_slide')
+   nextSlide.classList.add('current_slide')
+} 
+)
 // Button to move carousel right//
 nextButton.addEventListener('click', e => {
     let currentSlide = document.querySelector('.current_slide');
     let nextSlide = currentSlide.nextElementSibling;
     let moveAmount = nextSlide.style.left;
-    caroTrack.style.transform = 'translateX(-'+ moveAmount +')';
+    carouselTrack.style.transform = 'translateX(-'+ moveAmount +')';
     currentSlide.classList.remove('current_slide')
     nextSlide.classList.add('current_slide')
  } 
