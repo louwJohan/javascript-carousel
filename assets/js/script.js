1bfbc3557b2808ee38407c0c12747e0ba9c54a06
Louw Johan Coetzee <Louwjohanc@gmail.com>
2021-12-21 21:10:35 +0000

    Add code to make slides move

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 let carouselSlides = document.getElementsByClassName("carousel");
-let nextButton = document.getElementsByClassName('button_right');
-let prevButton = document.getElementsByClassName('button_left')
+let nextButton = document.querySelector('.button_right');
+let prevButton = document.querySelector('.button_left')
 let slideWidth = carouselSlides[0].clientWidth;
+let caroTrack = document.querySelector('.carousel_track');
+
 
 for (let i = 0; i < carouselSlides.length; i++){
-    carouselSlides[i].style.left = slideWidth * i +'px';
+   carouselSlides[i].style.left = slideWidth * i +'px';
 }
 
 // Button to move carousel right//
 nextButton.addEventListener('click', e => {
-    let currentSlide = document.getElementsByClassName('current_slide');
-    console.log(currentSlide);
+    let currentSlide = document.querySelector('.current_slide');
+    let nextSlide = currentSlide.nextElementSibling;
+    let moveAmount = nextSlide.style.left;
+    caroTrack.style.transform = 'translateX(-'+ moveAmount +')';
+    currentSlide.classList.remove('current_slide')
+    nextSlide.classList.add('current_slide')
  } 
 )
